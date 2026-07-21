1b397627aa8a7bdd4c4aa40522ef52d2b5753de5
chenarrr <chenar.breshk@gmail.com>
2026-07-21 11:51:47 +0300

    cmd: add operator entrypoint (part 2/2)

--- cmd/main.go
+++ cmd/main.go
@@ -72,3 +72,78 @@ func main() {
 		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "poddoctor.poddoctor.dev",
+	}
+	if watchNamespace != "" {
+		ctrlOptions.Cache = cache.Options{
+			DefaultNamespaces: map[string]cache.Config{
+				watchNamespace: {},
+			},
+		}
+		setupLog.Info("restricting watch scope", "namespace", watchNamespace)
+	} else {
+		setupLog.Info("watching all namespaces")
+	}
+
+	restConfig := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(restConfig, ctrlOptions)
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	clientSet, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		setupLog.Error(err, "unable to create clientset")
+		os.Exit(1)
+	}
+
+	if err = (&controller.PodDiagnosisReconciler{
+		Client:        mgr.GetClient(),
+		Scheme:        mgr.GetScheme(),
+		ClientSet:     clientSet,
+		Recorder:      mgr.GetEventRecorder("poddoctor-controller"),
+		LogTailLines:  logTailLines,
+		RolloutWindow: rolloutWindow,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PodDiagnosis")
+		os.Exit(1)
+	}
+
+	if dashboardAddr != "0" {
+		srv := &http.Server{Addr: dashboardAddr, Handler: dashboard.Handler(mgr.GetAPIReader())}
+		if err := mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
+			errCh := make(chan error, 1)
+			go func() { errCh <- srv.ListenAndServe() }()
+			select {
+			case <-ctx.Done():
+				return srv.Shutdown(context.Background())
+			case err := <-errCh:
+				if errors.Is(err, http.ErrServerClosed) {
+					return nil
+				}
+				return err
+			}
+		})); err != nil {
+			setupLog.Error(err, "unable to add dashboard")
+			os.Exit(1)
+		}
+		setupLog.Info("dashboard enabled", "bindAddress", dashboardAddr)
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
